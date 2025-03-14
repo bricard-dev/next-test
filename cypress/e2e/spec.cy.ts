4b1e8b099e3bb1c909e0afb59b7fda1c7ad40092
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-03-14 18:46:26 +0100

    Refactor end-to-end testing scripts in package.json to ensure Prisma test runs before building the Next.js app. Enhance Cypress tests by adding checks for the existence and length of the users list. Add a screenshot for a failed test case for better debugging.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,5 +3,10 @@ describe("template spec", () => {
     cy.visit("/");
 
     cy.get("h1").should("contain", "Hello World");
+    cy.get("[data-testid='users-list']").should("exist");
+
+    cy.get("[data-testid='users-list']").within(() => {
+      cy.get("li").should("have.length", 2);
+    });
   });
 });
